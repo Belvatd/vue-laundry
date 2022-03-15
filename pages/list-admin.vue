2b619fb3c69581f3b0e826d38ef421efced65593
Belvatd <belva_talitha_28rpl@telkomschools.sch.id>
2022-03-15 23:31:50 +0700

    rapiin kode

--- pages/list-admin.vue
+++ pages/list-admin.vue
@@ -112,23 +112,6 @@
                 </v-card-actions>
               </v-card>
             </v-dialog>
-
-            <!-- MODAL DELETE -->
-            <v-dialog v-model="dialogDelete" max-width="500px">
-              <v-card>
-                <v-card-title class="text-h5"
-                  >Are you sure you want to delete this item?</v-card-title
-                >
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" text @click="closeDelete"
-                    >Cancel</v-btn
-                  >
-                  <v-btn color="blue darken-1" text>OK</v-btn>
-                  <v-spacer></v-spacer>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
           </v-toolbar>
         </template>
 
@@ -173,12 +156,6 @@ export default {
       password: "",
       role: "",
     },
-    defaultItem: {
-      id: "",
-      nama_user: "",
-      username: "",
-      role: "",
-    },
     isAdmin: false,
     notFound: false,
     data: {},
@@ -198,9 +175,6 @@ export default {
     },
     dialogEdit(val) {
       val || this.closeEdit();
-    },
-    dialogDelete(val) {
-      val || this.closeDelete();
     },
   },
 
@@ -326,20 +300,6 @@ export default {
 
     closeEdit() {
       this.dialogEdit = false;
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
-    },
-
-    deleteItem(item) {
-      this.editedIndex = this.tableValues.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialogDelete = true;
-    },
-
-    closeDelete() {
-      this.dialogDelete = false;
       this.$nextTick(() => {
         this.editedItem = Object.assign({}, this.defaultItem);
         this.editedIndex = -1;
--- pages/list-member.vue
+++ pages/list-member.vue
@@ -112,23 +112,6 @@
                 </v-card-actions>
               </v-card>
             </v-dialog>
-
-            <!-- MODAL DELETE -->
-            <!-- <v-dialog v-model="dialogDelete" max-width="500px">
-              <v-card>
-                <v-card-title class="text-h5"
-                  >Are you sure you want to delete this item?</v-card-title
-                >
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" text @click="closeDelete"
-                    >Cancel</v-btn
-                  >
-                  <v-btn color="blue darken-1" text>OK</v-btn>
-                  <v-spacer></v-spacer>
-                </v-card-actions>
-              </v-card>
-            </v-dialog> -->
           </v-toolbar>
         </template>
 
@@ -175,12 +158,6 @@ export default {
       jenis_kelamin: "",
       telp: "",
     },
-    // defaultItem: {
-    //   id: "",
-    //   nama_user: "",
-    //   username: "",
-    //   role: "",
-    // },
     isAdmin: false,
     notFound: false,
     data: {},
@@ -201,9 +178,6 @@ export default {
     dialogEdit(val) {
       val || this.closeEdit();
     },
-    // dialogDelete(val) {
-    //   val || this.closeDelete();
-    // },
   },
 
   created() {
@@ -322,7 +296,6 @@ export default {
     closeEdit() {
       this.dialogEdit = false;
       this.$nextTick(() => {
-        // this.editedItem = Object.assign({}, this.defaultItem);
         this.editedItem = Object.assign({});
         this.editedIndex = -1;
       });
@@ -333,14 +306,6 @@ export default {
       this.editedItem = Object.assign({}, item);
       this.dialogDelete = true;
     },
-
-    // closeDelete() {
-    //   this.dialogDelete = false;
-    //   this.$nextTick(() => {
-    //     this.editedItem = Object.assign({}, this.defaultItem);
-    //     this.editedIndex = -1;
-    //   });
-    // },
   },
 };
 </script>
